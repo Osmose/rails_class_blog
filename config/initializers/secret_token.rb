ead6fbb7b7e56e73d1f26d931b80bce616c23138
vagrant <vagrant@vagrant-ubuntu.hsd1.ca.comcast.net>
2011-02-03 22:05:22 -0800

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '24c975cc8e22a1c90d3afc67e16e24582bb9e07e7e6090bf23f965c34f46de7d616bb8567c027e9065a140a1acbfcef6a0f8950347ff147ef1374dad34f9ed75'
